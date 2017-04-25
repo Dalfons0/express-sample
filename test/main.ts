76e8c57279b96e5f25c1727adea235b868153dad
Alfonso Garcia <david.alfonso-garcia@capgemini.com>
2017-04-25 13:23:41 +0200

    README update

--- test/main.ts
+++ test/main.ts
@@ -2,8 +2,8 @@ import { expect } from 'chai'
 import business from '../src/logic'
 
 describe('greetings', () => {
-    it('shouldd say: "Hello logic world!"', (done: Function) => {
-        business.greetings(1000).then(function (result: string) {
+    it('should say: "Hello logic world!"', (done: Function) => {
+        business.greetings(1000).then((result: string) => {
             expect(result).to.be.a("string")
             expect(result).to.be.equal("Hello logic world!")
             done()
@@ -11,7 +11,6 @@ describe('greetings', () => {
         .catch((err: Error) => {
             done(err)
         })
-        
     })
 })
 
